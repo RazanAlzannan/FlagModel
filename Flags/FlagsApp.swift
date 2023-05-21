6073c72b0b281ac95bcdbe335143a913a523ab14
Razan <razanalzannan@gmail.com>
2023-05-22 02:53:41 +0300

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Razan Mohammed Alzannan on 02/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
