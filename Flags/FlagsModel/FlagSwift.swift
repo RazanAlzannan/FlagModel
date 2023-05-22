146e976361c9999dbb7eb5bf83182eddb9e99b00
Razan <razanalzannan@gmail.com>
2023-05-22 05:11:27 +0300

    Updated

---
+++ Flags/FlagsModel/FlagSwift.swift
@@ -0,0 +1,8 @@
+//
+//  FlagSwift.swift
+//  Flags
+//
+//  Created by Razan Mohammed Alzannan on 02/11/1444 AH.
+//
+
+import Foundation
---
+++ Flags/FlagsModel/FlagsModel.swift
@@ -0,0 +1,20 @@
+//
+//  FlagsModel.swift
+//  Flags
+//
+//  Created by Razan Mohammed Alzannan on 02/11/1444 AH.
+//
+
+import SwiftUI
+
+struct FlagsModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FlagsModel_Previews: PreviewProvider {
+    static var previews: some View {
+        FlagsModel()
+    }
+}
